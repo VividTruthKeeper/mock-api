ab4e954d05085065b8546051b674f7782fc76b72
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-02-03 00:03:07 +0500

    get current user route

--- src/routes/current-user.ts
+++ src/routes/current-user.ts
@@ -1,9 +1,30 @@
 import express from "express";
+import { TokenInvalidError } from "../errors/user/token-invalid-error";
+import { UserNotFound } from "../errors/user/user-not-found";
+import { authVerify } from "../middlewares/auth-verify";
+import { User } from "../models/user";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there!");
+router.get("/api/users/currentUser", async (req, res) => {
+  const { token } = req.query;
+
+  if (!token) {
+    throw new TokenInvalidError();
+  }
+
+  const { userId } = await authVerify(token);
+
+  const userByUserId = await User.findOne({ where: { userId: userId } });
+
+  if (userByUserId) {
+    res.status(200).send({
+      status: "success",
+      data: userByUserId.get(),
+    });
+  } else {
+    throw new UserNotFound();
+  }
 });
 
 export { router as currentUserRouter };
