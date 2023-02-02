4839314ddaf11f64b052ebeff5deacf6f0b49ad9
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-02-03 00:02:53 +0500

    return type

--- src/middlewares/auth-verify.ts
+++ src/middlewares/auth-verify.ts
@@ -1,13 +1,20 @@
 import jwt from "jsonwebtoken";
 import { TokenInvalidError } from "../errors/user/token-invalid-error";
 
-export const authVerify = (token: string) => {
-  const decodedToken = jwt.verify(token, process.env.TOKEN_KEY || "");
+interface IdecodedTokenType {
+  userId: string;
+  email: string;
+  iat: number;
+  exp: number;
+}
+
+export const authVerify = async (token: any): Promise<IdecodedTokenType> => {
+  const decodedToken = await jwt.verify(token, process.env.TOKEN_KEY || "");
   if (!(decodedToken instanceof Object)) {
     throw new TokenInvalidError();
   }
-  if (!("user_id" in decodedToken)) {
-    throw new TokenInvalidError('Token is missing a required field "userId"');
+  if (!("userId" in decodedToken)) {
+    throw new TokenInvalidError();
   }
-  return decodedToken;
+  return decodedToken as IdecodedTokenType;
 };
