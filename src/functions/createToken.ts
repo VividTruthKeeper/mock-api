406e5b01e4bab53955b2f68b7858b7816d48b684
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-02-02 23:19:41 +0500

    dynamic exp period

--- src/functions/createToken.ts
+++ src/functions/createToken.ts
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 
 export const createToken = (userId: string, email: string): string => {
-  return jwt.sign({ user_id: userId, email }, process.env.TOKEN_KEY || "", {
-    expiresIn: "2h",
+  return jwt.sign({ userId: userId, email }, process.env.TOKEN_KEY || "", {
+    expiresIn: process.env.TOKEN_EXPIRES_IN || "2h",
   });
 };
