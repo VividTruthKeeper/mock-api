e6acd120b32a501bb3fc7d3176549f6487721e46
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-02-02 17:01:20 +0500

    user id column in db

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,10 +1,19 @@
 import { Table, Column, DataType, Model } from "sequelize-typescript";
+import { v4 as uuidv4 } from "uuid";
 
 @Table({
   timestamps: false,
   tableName: "users",
 })
 export class User extends Model {
+  @Column({
+    type: DataType.UUID,
+    allowNull: false,
+    unique: true,
+    defaultValue: uuidv4(),
+  })
+  userId!: number;
+
   @Column({
     type: DataType.STRING,
     allowNull: false,
@@ -14,11 +23,12 @@ export class User extends Model {
   @Column({
     type: DataType.STRING,
     allowNull: false,
+    unique: true,
   })
   email!: string;
 
   @Column({
-    type: DataType.STRING,
+    type: DataType.STRING(64),
     allowNull: false,
   })
   hash!: string;
