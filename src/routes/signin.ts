850e5a6e345d21d7ac5d6d6f06a33214f0eaa662
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-02-02 17:10:23 +0500

    email field check

--- src/routes/signin.ts
+++ src/routes/signin.ts
@@ -1,36 +1,42 @@
-import express from "express";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
 import { comparePassword } from "../functions/comparePassword";
 import { User } from "../models/user";
+import { RequestValidationError } from "../errors/request-validation-error";
 
 const router = express.Router();
 
-router.post("/api/users/signin", async (req, res) => {
-  const { email, password } = req.body;
-  let passwordCorrect: boolean = false;
-  try {
+router.post(
+  "/api/users/signin",
+  [body("email").isEmail().withMessage("Invalid email")],
+  async (req: Request, res: Response) => {
+    // req params error
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      throw new RequestValidationError(errors.array());
+    }
+
+    const { email, password } = req.body;
+    let passwordCorrect: boolean = false;
+
     const userByEmail = await User.findOne({ where: { email: email } });
     if (userByEmail) {
-      console.log(userByEmail);
       const result = await userByEmail.get();
       passwordCorrect = await comparePassword(password, result.hash);
     }
-  } catch (err) {
-    console.log(err);
-    throw new DatabaseConnectionError();
-  }
 
-  if (passwordCorrect) {
-    res.send({
-      status: "success",
-      message: "Signed in successfuly",
-    });
-  } else {
-    res.send({
-      status: "failed",
-      message: "Incorrect password",
-    });
+    if (passwordCorrect) {
+      res.send({
+        status: "success",
+        message: "Signed in successfuly",
+      });
+    } else {
+      res.send({
+        status: "failed",
+        message: "Incorrect password",
+      });
+    }
   }
-});
+);
 
 export { router as signinRouter };
