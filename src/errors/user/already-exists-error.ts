d188a6ca7b203370c2d48730eb3c67f511facff0
VividTruthKeeper <mr.aydogdy02@gmail.com>
2023-02-02 17:20:25 +0500

    'field' field

--- src/errors/user/already-exists-error.ts
+++ src/errors/user/already-exists-error.ts
@@ -3,12 +3,13 @@ import { CustomError } from "../custom-error";
 export class AlreadyExistsError extends CustomError {
   statusCode = 409;
   reason = "User with this email already exists";
+  field = "email";
   constructor() {
     super("User with this email already exists");
     Object.setPrototypeOf(this, AlreadyExistsError.prototype);
   }
 
   serializeErrors() {
-    return [{ message: this.reason }];
+    return [{ message: this.reason, field: this.field }];
   }
 }
